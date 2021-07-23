e1a92fca21ed167ba82b9ddf733455647f4c3b2f
Chiara Campalani <campalanichiara@gmail.com>
2021-07-23 13:15:30 +0900

    created custom hook to handle API call to fetch movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,40 +5,12 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // API
 import API from '../API';
 // Custom Hooks
-
+import { useHomeFetch } from '../hooks/useHomeFetch';
 // Image
 import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
-  const [state, setState] = useState();
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-
-  const fetchMovies = async (page, searchTerm = "") => {
-    try {
-      setError(false);
-      setLoading(true);
-
-      const movies = await API.fetchMovies(searchTerm, page);
-      // console.log(movies);
-      
-      // calling state setter with function automatically uses previous state render == prev
-      // using double brackets: wrapping curly brackets into round ones to have function return an object!
-      setState(prev => ({
-        ...movies,
-        results:
-          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-      }))
-
-    } catch (error) {
-      setError(true);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchMovies(1);
-  }, []) // specify [] to have useEffect run only on initial render
+  const { state, loading, error } = useHomeFetch();
 
   console.log(state);
 
---
+++ src/hooks/useHomeFetch.js
@@ -0,0 +1,48 @@
+import { useState, useEffect, useRef } from 'react';
+// no need to import React as we are not returning any JSX
+import API from '../API';
+
+//creating an initial State => useful for when you will need to reset it
+const initialState = {
+  page: 0,
+  results: [],
+  total_pages: 0,
+  total_results: 0
+};
+
+
+export const useHomeFetch = () => {
+  const [state, setState] = useState(initialState);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const fetchMovies = async (page, searchTerm = "") => {
+    try {
+      setError(false);
+      setLoading(true);
+
+      const movies = await API.fetchMovies(searchTerm, page);
+      // console.log(movies);
+      
+      // calling state setter with function automatically uses previous state render == prev
+      // using double brackets: wrapping curly brackets into round ones to have function return an object!
+      setState(prev => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
+      }))
+
+    } catch (error) {
+      setError(true);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, []) // specify [] to have useEffect run only on initial render
+
+  return { state, loading, error }; // no need to specify state: state to return state itself if it has the same name
+}
+
+// export default useHomeFetch;
