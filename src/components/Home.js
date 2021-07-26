1d3aa812e6431e76289aedee3fe1feda8f204b2f
Chiara Campalani <campalanichiara@gmail.com>
2021-07-26 23:29:01 +0900

    created Grid & Thumb components + styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from 'react';
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 // Components
 import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
 // API
 import API from '../API';
 // Custom Hooks
@@ -24,6 +26,18 @@ const Home = () => {
           text={state.results[0].overview}
         />
       }
+      <Grid header='Popular Movies' >
+        {state.results.map(movie => (
+          <Thumb 
+            key={movie.id}
+            clickable // defaults to true
+            image={movie.poster_path
+              ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+              : NoImage}
+            movieId = {movie.id}
+          />
+        ))}
+      </Grid>
     </>
   );
 };
